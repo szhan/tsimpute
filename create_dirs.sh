21f082e66519cac859d12e51c575f0fc2de8d53b
Shing Zhan <shing.zhan@gmail.com>
2022-03-18 10:58:12 +0000

    Add bash script to prepare dirs

---
+++ create_dirs.sh
@@ -0,0 +1,19 @@
+base_dir="data/modern_panmictic_p1000_2/"
+
+ref_dir=${base_dir}"ref/"
+miss_dir=${base_dir}"miss/"
+true_dir=${base_dir}"true/"
+ts_anc_ref_dir=${base_dir}"ts_anc_ref/"
+imputed_tsinfer_dir=${base_dir}"imputed_tsinfer/"
+imputed_tsonly_dir=${base_dir}"imputed_tsonly/"
+imputed_beagle_dir=${base_dir}"imputed_beagle/"
+
+mkdir -p ${base_dir}
+mkdir -p ${ref_dir}
+mkdir -p ${miss_dir}
+mkdir -p ${true_dir}
+mkdir -p ${ts_anc_ref_dir}
+mkdir -p ${imputed_tsinfer_dir}
+mkdir -p ${imputed_tsonly_dir}
+mkdir -p ${imputed_beagle_dir}
+
