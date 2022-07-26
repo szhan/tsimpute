9902b672e0e0c2b8ca8676a759a432ad8d673c1a
Shing Zhan <shing.zhan@gmail.com>
2022-07-26 16:53:27 +0100

    Refactor

--- python/impute_using_ts_only.py
+++ python/impute_using_ts_only.py
@@ -23,55 +23,6 @@ start_datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
 
 ### Helper functions
-def compare_sites_sd_and_ts(
-    sample_data, tree_sequence, is_common, check_matching_ancestral_state=True
-):
-    """
-    If `is_common` is set to True, then get the ids and positions of the sites
-    found in `sample_data` AND in `tree_sequence`.
-
-    if `is_common` is set to False, then get the ids and positions of the sites
-    found in `sample_data` but NOT in `tree_sequence`.
-
-    :param TreeSequence tree_sequence:
-    :param SampleData sample_data:
-    :param is_common bool:
-    :param check_matching_ancestral_state bool: (default=True)
-    :return tuple: tuple of two ndarray
-    """
-    ts_site_positions = np.empty(tree_sequence.num_sites)
-
-    i = 0
-    for v in tree_sequence.variants():
-        ts_site_positions[i] = v.site.position
-        i += 1
-
-    assert i == tree_sequence.num_sites
-
-    sd_site_ids = []
-    sd_site_positions = []
-    for sd_v in sample_data.variants():
-        if is_common:
-            if sd_v.site.position in ts_site_positions:
-                sd_site_ids.append(sd_v.site.id)
-                sd_site_positions.append(sd_v.site.position)
-                if check_matching_ancestral_state:
-                    ts_site = tree_sequence.site(position=sd_v.site.position)
-                    assert sd_v.site.ancestral_state == ts_site.ancestral_state, (
-                        f"Ancestral states at {sd_v.site.position} not the same, "
-                        + f"{sd_v.site.ancestral_state} vs. {ts_site.ancestral_state}."
-                    )
-        else:
-            if sd_v.site.position not in ts_site_positions:
-                sd_site_ids.append(sd_v.site.id)
-                sd_site_positions.append(sd_v.site.position)
-
-    return (
-        np.array(sd_site_ids),
-        np.array(sd_site_positions),
-    )
-
-
 def make_compatible_sample_data(sample_data, ancestors_ts):
     """
     Make an editable copy of a `sample_data` object, and edit it so that:
