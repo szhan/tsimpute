a24639472d6547832e7b9921eee1f494db4dfede
Shing Zhan <shing.zhan@gmail.com>
2022-08-09 06:55:41 +0700

    Update

--- bash/make_cmds_ceu_chb_p10.sh
+++ bash/make_cmds_ceu_chb_p10.sh
@@ -2,6 +2,6 @@ base_dir="analysis/genealogy_only/ten_ceu_chb_t0_p10/"
 
 for i in {1..100}
 do
-    csv_file=${base_dir}"/i"${i}".csv"
-    echo "python python/main.py -i "${i}" -t 0 -p 0.10 -m ten_pop --pop_ref CEU --pop_query CHB --out_prefix "${csv_file}
+    prefix=${base_dir}"/i"
+    echo "python python/main.py -i "${i}" -t 0 -p 0.10 -m ten_pop --pop_ref CEU --pop_query CHB --out_prefix "${prefix}
 done
--- bash/make_cmds_yri_ceu_p10.sh
+++ bash/make_cmds_yri_ceu_p10.sh
@@ -2,6 +2,6 @@ base_dir="analysis/genealogy_only/ten_yri_ceu_t0_p10/"
 
 for i in {1..100}
 do
-    csv_file=${base_dir}"/i"${i}".csv"
-    echo "python python/main.py -i "${i}" -t 0 -p 0.10 -m ten_pop --pop_ref YRI --pop_query CEU --out_prefix "${csv_file}
+    prefix=${base_dir}"/i"
+    echo "python python/main.py -i "${i}" -t 0 -p 0.10 -m ten_pop --pop_ref YRI --pop_query CEU --out_prefix "${prefix}
 done
