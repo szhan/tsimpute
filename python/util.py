74e2549b759967be6a7c2c18f42e2d1db38516e4
Shing Zhan <shing.zhan@gmail.com>
2022-09-09 17:05:07 +0100

    Refactor

--- python/util.py
+++ python/util.py
@@ -8,49 +8,53 @@ def count_sites_by_type(ts_or_sd):
     Iterate through the variants of a `TreeSequence` or `SampleData` object,
     and count the number of mono-, bi-, tri-, and quad-allelic sites.
 
-    :param TreeSequence/SampleData ts_or_sd:
+    :param tskit.TreeSequence/tsinfer.SampleData ts_or_sd:
     :return None:
     """
     assert isinstance(ts_or_sd, (tskit.TreeSequence, tsinfer.SampleData))
 
-    sites_mono = 0
-    sites_bi = 0
-    sites_bi_singleton = 0
-    sites_tri = 0
-    sites_quad = 0
+    num_sites_mono = 0
+    num_sites_bi = 0
+    num_sites_bi_singleton = 0
+    num_sites_tri = 0
+    num_sites_quad = 0
 
     for v in ts_or_sd.variants():
         num_alleles = len(set(v.alleles) - {None})
         if num_alleles == 1:
-            sites_mono += 1
+            num_sites_mono += 1
         elif num_alleles == 2:
-            sites_bi += 1
+            num_sites_bi += 1
             if np.sum(v.genotypes) == 1:
-                sites_bi_singleton += 1
+                num_sites_bi_singleton += 1
         elif num_alleles == 3:
-            sites_tri += 1
+            num_sites_tri += 1
         else:
-            sites_quad += 1
+            num_sites_quad += 1
 
-    sites_total = sites_mono + sites_bi + sites_tri + sites_quad
+    num_sites_total = num_sites_mono + num_sites_bi + num_sites_tri + num_sites_quad
 
-    print(f"\tsites mono : {sites_mono}")
-    print(f"\tsites bi   : {sites_bi} ({sites_bi_singleton} singletons)")
-    print(f"\tsites tri  : {sites_tri}")
-    print(f"\tsites quad : {sites_quad}")
-    print(f"\tsites total: {sites_total}")
+    print(f"\tsites mono : {num_sites_mono}")
+    print(f"\tsites bi   : {num_sites_bi} ({num_sites_bi_singleton} singletons)")
+    print(f"\tsites tri  : {num_sites_tri}")
+    print(f"\tsites quad : {num_sites_quad}")
+    print(f"\tsites total: {num_sites_total}")
 
     return None
 
 
-def check_site_positions_ts_issubset_sd(tree_sequence, sample_data):
+def check_site_positions_ts_issubse_sd(
+    tree_sequence,
+    sample_data
+):
     """
-    Check whether the site positions in `TreeSequence` are a subset of
-    the site positions in `SampleData`.
+    Check whether the site positions in `tskit.TreeSequence` are a subset of
+    the site positions in `tsinfer.SampleData`.
 
-    :param TreeSequence tree_sequence:
-    :param SampleData sample_data:
-    :return bool:
+    :param tskit.TreeSequence tree_sequence:
+    :param tsinfer.SampleData sample_data:
+    :return:
+    :rtype: bool
     """
     ts_site_positions = np.empty(tree_sequence.num_sites)
     sd_site_positions = np.empty(sample_data.num_sites)
@@ -75,7 +79,10 @@ def check_site_positions_ts_issubset_sd(tree_sequence, sample_data):
 
 
 def compare_sites_sd_and_ts(
-    sample_data, tree_sequence, is_common, check_matching_ancestral_state=True
+    sample_data,
+    tree_sequence,
+    is_common,
+    check_matching_ancestral_state=True
 ):
     """
     If `is_common` is set to True, then get the ids and positions of the sites
@@ -88,7 +95,8 @@ def compare_sites_sd_and_ts(
     :param SampleData sample_data:
     :param is_common bool:
     :param check_matching_ancestral_state bool: (default=True)
-    :return tuple: tuple of two ndarray
+    :return:
+    :rtype: 2-tuple of np.array
     """
     ts_site_positions = np.empty(tree_sequence.num_sites)
 
