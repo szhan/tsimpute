0436f4dc537b51adecdf8b3a1cb199c2695ab000
Shing Zhan <shing.zhan@gmail.com>
2022-09-10 14:00:20 +0100

    Download CSI file as well

--- bash/download_gvf_from_ensembl.sh
+++ bash/download_gvf_from_ensembl.sh
@@ -6,17 +6,25 @@ mkdir -p ${dest_dir}
 
 base_url="http://ftp.ensembl.org/pub/release-107/variation/vcf/homo_sapiens/"
 vcf_file="homo_sapiens-chr20.vcf.gz"
+csi_file=${vcf_file}".csi"
 
 cd ${dest_dir}
 curl ${base_url}"README" -O
 curl ${base_url}"CHECKSUMS" -O
 curl ${base_url}"/"${vcf_file} -O
+curl ${base_url}"/"${csi_file} -O
 
 vcf_file_pattern=${vcf_file}"$"
-sum_src=`grep ${vcf_file_pattern} CHECKSUMS`
-sum_dest=$(sum ${vcf_file})
+csi_file_pattern=${csi_file}"$"
 
-echo "SUM SRC="${sum_src}
-echo "SUM DEST="${sum_dest}
+sum_vcf_src=`grep ${vcf_file_pattern} CHECKSUMS`
+sum_csi_src=`grep ${csi_file_pattern} CHECKSUMS`
+sum_vcf_dest=$(sum ${vcf_file})
+sum_csi_dest=$(sum ${csi_file})
+
+echo "SUM VCF SRC="${sum_vcf_src}
+echo "SUM VCF DEST="${sum_vcf_dest}
+echo "SUM CSI SRC="${sum_csi_src}
+echo "SUM CSI DEST="${sum_csi_dest}
 
 cd ..
