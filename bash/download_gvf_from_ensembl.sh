5ac25965d3ab6359fb1927af0a661da1d4b7c464
Shing Zhan <shing.zhan@gmail.com>
2022-09-10 09:36:20 +0100

    Download VCF file instead of GVF file

--- bash/download_gvf_from_ensembl.sh
+++ bash/download_gvf_from_ensembl.sh
@@ -4,17 +4,18 @@ dest_dir=${base_dir}"/ensembl"
 mkdir -p ${base_dir}
 mkdir -p ${dest_dir}
 
-readme_url="http://ftp.ensembl.org/pub/release-107/variation/gvf/homo_sapiens/README"
-chk_url="http://ftp.ensembl.org/pub/release-107/variation/gvf/homo_sapiens/CHECKSUMS"
-gvf_url="http://ftp.ensembl.org/pub/release-107/variation/gvf/homo_sapiens/homo_sapiens-chr20.gvf.gz"
+base_url="http://ftp.ensembl.org/pub/release-107/variation/vcf/homo_sapiens/"
+readme_url=${base_url}"README"
+chk_url=${base_url}"CHECKSUMS"
+gvf_url=${base_url}"homo_sapiens-chr20.vcf.gz"
 
 cd ${dest_dir}
 curl ${readme_url} -O
 curl ${chk_url} -O
 curl ${gvf_url} -O
 
-sum_src=`grep 'README' CHECKSUMS`
-sum_dest=`sum README`
+sum_src=`grep 'vcf.gz' CHECKSUMS`
+sum_dest=`sum *vcf.gz`
 
 echo "SUM SRC="${sum_src}
 echo "SUM DEST="${sum_dest}
