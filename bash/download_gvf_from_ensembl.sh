6c991b3170eee4a07586d2121514b03dbea47c9e
Shing Zhan <shing.zhan@gmail.com>
2022-09-10 12:03:37 +0100

    Fix checksum checking

--- bash/download_gvf_from_ensembl.sh
+++ bash/download_gvf_from_ensembl.sh
@@ -5,17 +5,16 @@ mkdir -p ${base_dir}
 mkdir -p ${dest_dir}
 
 base_url="http://ftp.ensembl.org/pub/release-107/variation/vcf/homo_sapiens/"
-readme_url=${base_url}"README"
-chk_url=${base_url}"CHECKSUMS"
-gvf_url=${base_url}"homo_sapiens-chr20.vcf.gz"
+vcf_file="homo_sapiens-chr20.vcf.gz"
 
 cd ${dest_dir}
-curl ${readme_url} -O
-curl ${chk_url} -O
-curl ${gvf_url} -O
+curl ${base_url}"README" -O
+curl ${base_url}"CHECKSUMS" -O
+curl ${base_url}"/"${vcf_file} -O
 
-sum_src=`grep 'vcf.gz' CHECKSUMS`
-sum_dest=`sum *vcf.gz`
+vcf_file_pattern=${vcf_file}"$"
+sum_src=`grep ${vcf_file_pattern} CHECKSUMS`
+sum_dest=$(sum ${vcf_file})
 
 echo "SUM SRC="${sum_src}
 echo "SUM DEST="${sum_dest}
