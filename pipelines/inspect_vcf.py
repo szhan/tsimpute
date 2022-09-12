809b0b426ed5284f9267b626b863d5a5ad36ef58
Shing Zhan <shing.zhan@gmail.com>
2022-09-12 13:14:00 +0100

    Add argument num_threads

--- pipelines/inspect_vcf.py
+++ pipelines/inspect_vcf.py
@@ -44,12 +44,13 @@ import stats
     help="Right 0-based coordinate of the inclusion interval (default = None)."
     + "If None, then set to the last coordinate in the VCF file.",
 )
+@click.option("--num_threads", "-t", type=int, default=1, help="Number of threads (default = 1).")
 @click.option("--verbose", "-v", is_flag=True, help="Show warnings.")
 def parse_vcf_file(
-    in_vcf_file, out_csv_file, seq_name, left_coord, right_coord, verbose
+    in_vcf_file, out_csv_file, seq_name, left_coord, right_coord, num_threads, verbose
 ):
     vcf = read_vcf.get_vcf(
-        in_vcf_file, seq_name=seq_name, left_coord=left_coord, right_coord=right_coord
+        in_vcf_file, seq_name=seq_name, left_coord=left_coord, right_coord=right_coord, num_threads=num_threads
     )
     var_stats = stats.get_variant_statistics(vcf, show_warnings=verbose)
     stats.print_variant_statistics(var_stats, out_csv_file)
