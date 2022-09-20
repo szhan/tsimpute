60f89a44ea0fff7550ef060e0b9d1ab0f1c655ab
Shing Zhan <shing.zhan@gmail.com>
2022-09-20 21:42:32 +0100

    Refactor

--- python/measures.py
+++ python/measures.py
@@ -56,21 +56,21 @@ def compute_iqs_haploid(genotypes_true, genotypes_imputed):
     This specific formula is used to compute the IQS of imputed genotypes
     at biallelic sites in HAPLOID genomes.
 
-    :param np.array genotypes_true:
-    :param np.array genotypes_imputed:
+    :param np.array genotypes_true: List of alleles from ground-truth genotypes
+    :param np.array genotypes_impute List of alleles from imputed genotypesd:
     :return: IQS
     :rtype: float
     """
     assert len(genotypes_true) == len(genotypes_imputed), \
         f"Arrays of genotype are not of the same length."
 
-    # Allele 0 imputed correctly
+    # Allele 0 imputed correctly as allele 0
     n00 = np.sum([y == 0 for x, y in zip(genotypes_imputed, genotypes_true) if x == 0])
-    # Allele 1 imputed correctly
-    n11 = np.sum([y == 1 for x, y in zip(genotypes_imputed, genotypes_true) if x == 1])
-    # Allele 1 imputed wrongly
+    # Allele 0 imputed wrongly as allele 1
     n01 = np.sum([y == 1 for x, y in zip(genotypes_imputed, genotypes_true) if x == 0])
-    # Allele 1 imputed wrongly
+    # Allele 1 imputed correctly as allele 1
+    n11 = np.sum([y == 1 for x, y in zip(genotypes_imputed, genotypes_true) if x == 1])
+    # Allele 1 imputed wrongly as allele 0
     n10 = np.sum([y == 0 for x, y in zip(genotypes_imputed, genotypes_true) if x == 1])
 
     # Marginal counts
@@ -79,17 +79,19 @@ def compute_iqs_haploid(genotypes_true, genotypes_imputed):
     n_0 = n00 + n10
     n_1 = n01 + n11
 
-    # Total genotypes imputed
+    assert n0_ + n1_ == n_0 + n_1, f"Marginal counts do not add up."
+
+    # Total count
     n__ = n00 + n10 + n01 + n11
 
-    # Observed overall concordance
+    # Observed agreement (i.e. overall concordance)
     Po = float(n00 + n11) / float(n__)
 
     # Chance agreement
     Pc = float(n0_ * n_0 + n1_ * n_1) / float(n__ * n__)
 
-    assert Po >= 0 and Po <= 1
-    assert Pc >= 0 and Pc <= 1
+    assert Po >= 0 and Po <= 1, f"Po {Po} is not a proportion."
+    assert Pc >= 0 and Pc <= 1, f"Pc {Pc} is not a proportion."
 
     iqs = float("nan") if Pc == 1 else (Po - Pc) / (1 - Pc)
 
