5fc9cfd153bb0977832a8ade6bb15ed9a94494f7
Shing Zhan <shing.zhan@gmail.com>
2022-09-26 11:59:58 +0100

    Adjust focal region size

--- src/subset_sample.py
+++ src/subset_sample.py
@@ -13,9 +13,9 @@ sd = tsinfer.load(in_samples_file)
 # Subset by region
 # cytoband 20.p12.1
 chr = "20"
-length_focal_region = math.ceil(64_444_167 * 0.10)
+length_focal_region = 1e7 # 10 Mbp
 start = 11_900_000 # 1-based, inclusive
-end = 17_800_000 # 1-based, inclusive
+end = start + length_focal_region # 1-based, inclusive
 
 included_site_ids = []
 for s in sd.sites():
@@ -28,9 +28,8 @@ print(f"Region: {chr}:{start}-{end}")
 print(f"Sites: {len(included_site_ids)}")
 
 # Subset by individuals
-query_size = math.ceil(sd.num_individuals * 0.10)
-query_ids = np.random.choice(np.arange(sd.num_individuals), query_size)
-ref_ids = set(np.arange(sd.num_individuals)) - set(query_ids)
-
-sd_region_query = sd_region.subset(individuals=query_ids, path=out_query_samples_file)
-sd_region_ref = sd_region.subset(individuals=ref_ids, path=out_ref_samples_file)
+#query_size = math.ceil(sd.num_individuals * 0.10)
+#query_ids = np.random.choice(np.arange(sd.num_individuals), query_size)
+#ref_ids = set(np.arange(sd.num_individuals)) - set(query_ids)
+#sd_region_query = sd_region.subset(individuals=query_ids, path=out_query_samples_file)
+#sd_region_ref = sd_region.subset(individuals=ref_ids, path=out_ref_samples_file)
