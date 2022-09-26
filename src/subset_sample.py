e7bbcc65306f6116da4ffe1af635b359272091b7
Shing Zhan <shing.zhan@gmail.com>
2022-09-26 12:25:30 +0100

    Subset into ref. and target sets

--- src/subset_sample.py
+++ src/subset_sample.py
@@ -13,7 +13,7 @@ sd = tsinfer.load(in_samples_file)
 # Subset by region
 # cytoband 20.p12.1
 chr = "20"
-length_focal_region = 1e7 # 10 Mbp
+length_focal_region = 5e6 # 5 Mbp
 start = 11_900_000 # 1-based, inclusive
 end = start + length_focal_region # 1-based, inclusive
 
@@ -28,8 +28,13 @@ print(f"Region: {chr}:{start}-{end}")
 print(f"Sites: {len(included_site_ids)}")
 
 # Subset by individuals
-#query_size = math.ceil(sd.num_individuals * 0.10)
-#query_ids = np.random.choice(np.arange(sd.num_individuals), query_size)
-#ref_ids = set(np.arange(sd.num_individuals)) - set(query_ids)
-#sd_region_query = sd_region.subset(individuals=query_ids, path=out_query_samples_file)
-#sd_region_ref = sd_region.subset(individuals=ref_ids, path=out_ref_samples_file)
+query_size = math.ceil(sd.num_individuals * 0.10)
+query_ids = np.random.choice(sd.num_individuals, query_size, replace=False)
+ref_ids = list(set(np.arange(sd.num_individuals)) - set(query_ids))
+
+print(f"Full: {len(sd.num_individuals)}")
+print(f"Query: {len(query_ids)}")
+print(f"Ref: {len(ref_ids)}")
+
+sd_region_query = sd_region.subset(individuals=query_ids, path=out_query_samples_file)
+sd_region_ref = sd_region.subset(individuals=ref_ids, path=out_ref_samples_file)
