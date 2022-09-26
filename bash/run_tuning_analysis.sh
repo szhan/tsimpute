612d7fafc53c95dc17135b572ca5044fd025bc8d
Shing Zhan <shing.zhan@gmail.com>
2022-09-26 15:22:34 +0100

    Update output file prefix

--- bash/run_tuning_analysis.sh
+++ bash/run_tuning_analysis.sh
@@ -6,7 +6,7 @@ exponents=(-5 -4 -3 -2 -1 0 1 2 3 4)
 
 for i in ${exponents[@]}; do
     for j in ${exponents[@]}; do
-        prefix="a"$i"s"$j
+        prefix="a"$i"s"$j".1Mb"
         echo "python $script_file -r 1e-8 -a 1e$i -s 1e$j -i $in_file -o $out_dir -p $prefix -t 16"
     done
 done
