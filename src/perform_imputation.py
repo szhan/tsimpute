d901e60e6f88123dd758c5cdcd76429bac968645
Shing Zhan <shing.zhan@gmail.com>
2022-09-27 08:55:38 +0100

    Finish draft imputation script

--- src/perform_imputation.py
+++ src/perform_imputation.py
@@ -1,10 +1,7 @@
 import click
 from datetime import datetime
 import sys
-from pathlib import Path
-
 import numpy as np
-
 import tskit
 import tsinfer
 
@@ -14,37 +11,37 @@ import measures
 import util
 
 
-### Helper functions
 @click.command()
 @click.option(
-    "--in_reference_trees_file", "-r",
+    "--in_reference_trees_file",
+    "-r",
     type=click.Path(exists=True),
     required=True,
-    help="Input tree sequence file with reference samples."
+    help="Input tree sequence file with reference samples.",
 )
 @click.option(
-    "--in_target_samples_file", "-t",
+    "--in_target_samples_file",
+    "-t",
     type=click.Path(exists=True),
     required=True,
-    help="Input samples file with target samples."
+    help="Input samples file with target samples.",
 )
 @click.option(
-    "--in_chip_file", "-c",
+    "--in_chip_file",
+    "-c",
     type=click.Path(exists=True),
     required=True,
-    help="Input list of positions to retain before imputation."
+    help="Input list of positions to retain before imputation.",
 )
 @click.option(
-    "--out_dir", "-o",
+    "--out_dir",
+    "-o",
     type=click.Path(exists=True),
     required=True,
-    help="Output directory."
+    help="Output directory.",
 )
 @click.option(
-    "--out_prefix", "-p",
-    type=str,
-    required=True,
-    help="Prefix of the output file."
+    "--out_prefix", "-p", type=str, required=True, help="Prefix of the output file."
 )
 def run_pipeline(
     in_reference_trees_file,
@@ -58,9 +55,6 @@ def run_pipeline(
     ts_ref = tskit.load(in_reference_trees_file)
     sd_target = tsinfer.load(in_target_samples_file)
 
-    assert util.is_biallelic(ts_ref), f"Tree sequence has non-biallelic sites."
-    assert util.is_biallelic(sd_target), f"Sample data has non-biallelic sites."
-
     chip_site_pos = masks.parse_site_position_file(in_chip_file)
     mask_site_pos = []
 
@@ -72,8 +66,9 @@ def run_pipeline(
         if v.site.position not in chip_site_pos:
             mask_site_pos.append(v.site.position)
 
-    assert len(set(chip_site_pos) & set(mask_site_pos)) == 0, \
-        f"Chip and mask site positions are not mutually exclusive."
+    assert (
+        len(set(chip_site_pos) & set(mask_site_pos)) == 0
+    ), f"Chip and mask site positions are not mutually exclusive."
 
     sd_masked = masks.mask_sites_in_sample_data(
         sd_compat, sites=mask_site_pos, site_type="position"
@@ -81,40 +76,63 @@ def run_pipeline(
 
     ts_imputed = tsinfer.match_samples(sample_data=sd_masked, ancestors_ts=ts_anc)
 
+    assert (
+        v_ref.num_sites
+        == v_compat.num_sites
+        == v_masked.num_sites
+        == v_imputed.num_sites
+    ), f"Different number of sites in the tree sequences and sample data."
+
     ### Evaluate imputation performance
     results = None
+    num_non_biallelic_masked_sites = 0
+
     for v_ref, v_compat, v_masked, v_imputed in zip(
         ts_ref.variants(),  # Reference genomes from which to get the minor allele and MAF
-        sd_compat.variants(),  # Query genomes before site masking
-        sd_masked.variants(),  # Query genomes after site masking
+        sd_compat.variants(),  # Query genomes BEFORE site masking
+        sd_masked.variants(),  # Query genomes AFTER site masking
         ts_imputed.variants(),  # Query genomes with imputed sites
     ):
         if v_imputed.site.position in mask_site_pos:
+            if len(set(v_ref.alleles) - {None}) != 2:
+                num_non_biallelic_masked_sites += 1
+                continue
+
+            ref_ancestral_allele = v_ref.alleles[0]
+            ref_derived_allele = v_ref.alleles[1]
+
             # CHECK that ancestral states are identical.
-            assert (
-                v_ref.alleles[0] == sd_compat.sites_alleles[v_compat.site.id][0]
-            )
-            assert (
-                v_ref.alleles[0] == sd_masked.sites_alleles[v_masked.site.id][0]
-            )
-            assert v_ref.alleles[0] == v_imputed.alleles[0]
+            assert ref_ancestral_allele == sd_compat.sites_alleles[v_compat.site.id][0]
+            assert ref_ancestral_allele == sd_masked.sites_alleles[v_masked.site.id][0]
+            assert ref_ancestral_allele == v_imputed.alleles[0]
 
             assert set(v_masked.genotypes) == set([-1])
             assert not np.any(v_imputed.genotypes == -1)
 
-            # Note: A minor allele in `ts_ref` may be a major allele in `sd_query`.
-            freqs_ref = v_ref.frequencies()
-            af_0 = freqs_ref[v_ref.alleles[0]]
-            af_1 = freqs_ref[v_ref.alleles[1]]
-
-            # Get Minor Allele Index and Frequency from `ts_ref`.
+            # Get Minor Allele index and frequency from `ts_ref`.
             # Definition of a minor allele: MAF < 0.50.
-            if af_1 < af_0:
+            ref_freqs = v_ref.frequencies()
+            ref_af_0 = ref_freqs[ref_ancestral_allele]
+            ref_af_1 = ref_freqs[ref_derived_allele]
+
+            if ref_af_1 < ref_af_0:
                 ref_ma_index = 1
-                ref_ma_freq = af_1
+                ref_ma_freq = ref_af_1
             else:
                 ref_ma_index = 0
-                ref_ma_freq = af_0
+                ref_ma_freq = ref_af_0
+
+            # Get Minor Allele index and frequency from `ts_imputed`.
+            imputed_freqs = v_imputed.frequencies()
+            imputed_af_0 = imputed_freqs[ref_ancestral_allele]
+            imputed_af_1 = imputed_freqs[ref_derived_allele]
+
+            if imputed_af_1 < imputed_af_0:
+                imputed_ma_index = 1
+                imputed_ma_freq = imputed_af_1
+            else:
+                imputed_ma_index = 0
+                imputed_ma_freq = imputed_af_0
 
             # Assess imputation performance
             total_concordance = measures.compute_concordance(
@@ -126,12 +144,21 @@ def run_pipeline(
                 genotypes_imputed=v_imputed.genotypes,
             )
 
-            # line.shape = (1, 4)
+            # line.shape = (1, 7)
             line = np.array(
                 [
-                    [v_ref.site.position, ref_ma_index, ref_ma_freq, total_concordance, iqs],
+                    [
+                        v_ref.site.position,
+                        ref_ma_index,
+                        ref_ma_freq,
+                        imputed_ma_index,
+                        imputed_ma_freq,
+                        total_concordance,
+                        iqs,
+                    ],
                 ]
             )
+
             if results is None:
                 results = line
             else:
@@ -151,13 +178,26 @@ def run_pipeline(
                 "#" + "tsinfer" + "=" + f"{tsinfer.__version__}",
                 "#" + "size_ref" + "=" + f"{ts_ref.num_samples}",
                 "#" + "size_query" + "=" + f"{sd_compat.num_samples}",
+                "#" + "masked_sites" + "=" + f"{len(mask_site_pos)}",
+                "#"
+                + "non_biallelic_masked_sites"
+                + "="
+                + f"{num_non_biallelic_masked_sites}",
             ]
         )
         + "\n"
     )
 
     header_text += ",".join(
-        ["position", "ref_ma_index", "ref_ma_freq", "total_concordance", "iqs"]
+        [
+            "position",
+            "ref_ma_index",
+            "ref_ma_freq",
+            "imputed_ma_index",
+            "imputed_ma_freq",
+            "total_concordance",
+            "iqs",
+        ]
     )
 
     np.savetxt(
