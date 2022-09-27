130034c7925fddc4e78e8c5ade8c0f017d7457bc
Shing Zhan <shing.zhan@gmail.com>
2022-09-27 22:49:39 +0100

    Fix bug

--- viz/plot_mmr_exploratory_analysis.py
+++ viz/plot_mmr_exploratory_analysis.py
@@ -11,7 +11,7 @@ def parse_imputation_results_file(in_file):
     df_full = pd.read_csv(in_file, comment="#")
     df_full = df_full[df_full["iqs"].notna()]
     df_subset = df_full[df_full["ref_minor_allele_freq"] == 0]
-    assert df_subset.shape[0] == df_full.shape[0], \
+    assert df_subset.shape[0] == 0, \
         f"Some {df_subset.shape[0]} sites have no minor allele, " \
         f"but {df_full.shape[0]} sites should have a minor allele."
     mean_iqs = df_full["iqs"].mean(skipna=True)
