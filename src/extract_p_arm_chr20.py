bc3351d63bfb7d3f768a5a685578c98e28eb4efe
Shing Zhan <shing.zhan@gmail.com>
2022-09-29 19:36:52 +0100

    Store subsetted samples in file

--- src/extract_p_arm_chr20.py
+++ src/extract_p_arm_chr20.py
@@ -10,4 +10,4 @@ sd = tsinfer.load("../analysis/sisu42_ensembl_aa_high/chr20.samples")
 sd_p_site_pos = sd.sites_position[:][sd.sites_position[:] <= 25700000]
 len(sd_p_site_pos)                         
 
-sd_p = sd.subset(sites=np.arange(len(sd_p_site_pos)))
+sd_p = sd.subset(sites=np.arange(len(sd_p_site_pos)), path="chr20_p_arm.samples")
