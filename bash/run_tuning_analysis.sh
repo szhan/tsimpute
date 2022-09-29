81bd539d378bcd26c742e50c74b4180fe3d4a51d
Shing Zhan <shing.zhan@gmail.com>
2022-09-29 12:53:15 +0100

    Fix bug and expand parameter range

--- bash/run_tuning_analysis.sh
+++ bash/run_tuning_analysis.sh
@@ -2,15 +2,15 @@ in_file="../analysis/tuning/out.region.ref.samples"
 out_dir="../analysis/tuning/"
 script_file="src/run_tsinfer.py"
 
-exponents=(-5 -4 -3 -2 -1 0 1 2 3 4)
+exponents=(-7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7)
 
 cpus="1"
 
 for i in ${exponents[@]}; do
     for j in ${exponents[@]}; do
         prefix="a"$i"s"$j".1Mb"
-        out_file=${out_dir}${prefix}".1Mb.imputation.csv"
-        if [ -f ${out_file} ]; then
+        out_file=${out_dir}${prefix}".imputation.csv"
+        if [ !-f ${out_file} ]; then
             echo "python $script_file -r 1e-8 -a 1e$i -s 1e$j -i $in_file -o $out_dir -p $prefix -t $cpus"
         fi
     done
