c84fbada859953e4873cd7c29361af7a691bba10
Shing Zhan <shing.zhan@gmail.com>
2022-09-29 16:56:46 +0100

    Have separate files for imputing datasets used for tuning tsinfer parameters

---
+++ bash/run_imputation_tuning_leaves_removed.sh
@@ -0,0 +1,21 @@
+in_dir="../analysis/tuning/"
+out_dir="../analysis/tuning/imputed_leaves_removed/"
+script_file="./src/perform_imputation.py"
+
+in_target_file="../analysis/tuning/out.region.query.samples"
+in_chip_file="../data/fimm/chip_positions_chr20.txt"
+
+cpus="1"
+
+exponents=(-7 -6 -5 -4 -3 -2 -1 0 1 2 3 4)
+
+for i in ${exponents[@]}; do
+    for j in ${exponents[@]}; do
+        prefix="a"$i"s"$j".1Mb"
+        in_trees_file=${in_dir}${prefix}".inferred.trees"
+        out_file=${out_dir}${prefix}".imputation.csv"
+        if [ ! -f $out_file ]; then
+            echo "python $script_file -r 1e-8 -s 1e$j -i1 $in_trees_file -i2 $in_target_file -c $in_chip_file -o $out_dir -p $prefix --remove_leaves True -t $cpus"
+        fi
+    done
+done
