19d968c85532e40f1e2b47bd67c40a0a801c2459
Shing Zhan <shing.zhan@gmail.com>
2022-09-29 13:05:13 +0100

    Reduce parameter range

--- bash/run_tuning_analysis.sh
+++ bash/run_tuning_analysis.sh
@@ -2,7 +2,7 @@ in_file="../analysis/tuning/out.region.ref.samples"
 out_dir="../analysis/tuning/"
 script_file="src/run_tsinfer.py"
 
-exponents=(-7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7)
+exponents=(-7 -6 -5 -4 -3 -2 -1 0 1 2 3 4)
 
 cpus="1"
 
@@ -10,7 +10,7 @@ for i in ${exponents[@]}; do
     for j in ${exponents[@]}; do
         prefix="a"$i"s"$j".1Mb"
         out_file=${out_dir}${prefix}".inferred.trees"
-        if [[ ! -f ${out_file} ]]; then
+        if [ ! -f ${out_file} ]; then
             echo "python $script_file -r 1e-8 -a 1e$i -s 1e$j -i $in_file -o $out_dir -p $prefix -t $cpus"
         fi
     done
