31d2de5a996d0f4d0c197dcb865ef075d1c70db3
Shing Zhan <shing.zhan@gmail.com>
2022-09-30 18:57:15 +0100

    Rework

--- bash/make_cmds_ceu_ceu_p10.sh
+++ bash/make_cmds_ceu_ceu_p10.sh
@@ -1,7 +1,19 @@
-base_dir="analysis/genealogy_only/ten_ceu_ceu_t0_p10/"
+pop_ref=$1
+pop_query=$2
+perc_mask_sites=$3
+start_index=$4
+end_index=$5
 
-for i in {1..100}
-do
-    prefix=${base_dir}"/i"
-    echo "python python/main.py -i "${i}" -t 0 -p 0.10 -m ten_pop --pop_ref CEU --pop_query CEU --out_prefix "${prefix}
+script_file="./src/run_simulations.py"
+
+sampling_time="0"
+prefix="i"
+
+base_dir="./analysis/genealogy_only"
+out_dir=$base_dir"/ten_${pop_ref}_${pop_query}_t${sampling_time}_p${perc_mask_sites}"
+
+mkdir -p $base_dir
+
+for i in {$start_index..$end_index}; do
+    echo "python ${script_file} -i ${i} -t ${sampling_time} -p 0.${perc_mask_sites} -m ten_pop --pop_ref ${pop_ref} --pop_query ${pop_query} -o ${out_dir} -p ${prefix}"
 done
