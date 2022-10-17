efb05cd2d39bf601660939fc0b8b7db31e65a7ef
Shing Zhan <shing.zhan@gmail.com>
2022-10-17 19:50:06 +0100

    Add incomplete docstring, add sequence length arg, reformat, and refactor

--- src/extract_region_from_samples.py
+++ src/extract_region_from_samples.py
@@ -1,6 +1,6 @@
 import click
-import tsinfer
 import numpy as np
+import tsinfer
 
 
 @click.command()
@@ -19,14 +19,32 @@ import numpy as np
     help="Output samples file",
 )
 @click.option("--end", type=int, required=True, help="Right coordinate (1-based)")
-def extract_sites_from_region(in_samples_file, out_samples_file, end):
+@click.option(
+    "--sequence_length",
+    type=float,
+    default=None,
+    help="Set sequence length manually. If None, then set to 1 + end.",
+)
+def extract_sites_by_coordinates(in_samples_file, out_samples_file, end, sequence_length):
+    """
+    TODO: Subset using start coordinate.
+
+    :return: Subsetted samples.
+    :rtype: tsinfer.SampleData
+    """
     sd = tsinfer.load(in_samples_file)
+
     sd_subset_site_pos = sd.sites_position[:][sd.sites_position[:] < end]
+    seq_len = end + 1 if sequence_length is None else sequence_length
+
     sd_subset = sd.subset(
-        sites=np.arange(len(sd_subset_site_pos)), path=out_samples_file
+        sites=np.arange(len(sd_subset_site_pos)),
+        sequence_length=seq_len,
+        path=out_samples_file,
     )
+
     return sd_subset
 
 
 if __name__ == "__main__":
-    extract_sites_from_region()
+    extract_sites_by_coordinates()
