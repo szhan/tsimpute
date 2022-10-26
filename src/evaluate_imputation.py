f9ce749a9897b7171e62b65ab510c305e3bb5062
Shing Zhan <shing.zhan@gmail.com>
2022-10-26 15:24:03 +0100

    Remove unneeded code and clean up

--- src/evaluate_imputation.py
+++ src/evaluate_imputation.py
@@ -42,12 +42,6 @@ import measures
     help="Input tree sequence file with reference samples.",
 )
 @click.option(
-    "--remove_leaves",
-    type=bool,
-    required=True,
-    help="Remove leaves when making ancestors tree sequence.",
-)
-@click.option(
     "--in_chip_file",
     "-c",
     type=click.Path(exists=True),
@@ -79,24 +73,13 @@ import measures
     default=1.0,
     help="Maximum threshold on IQS.",
 )
-@click.option(
-    "--flip_alleles",
-    is_flag=True,
-    default=False,
-    help="Flip ancestral alleles and derived alleles in imputed genotypes before computing IQS.",
-)
 def evaluate_imputation(
     in_imputed_file,
     in_file_type,
     in_true_samples_file,
     in_reference_trees_file,
-    remove_leaves,
     in_chip_file,
     out_csv_file,
-    min_maf,
-    min_iqs,
-    max_iqs,
-    flip_alleles,
 ):
     start_dt = datetime.now()
     start_dt_str = start_dt.strftime("%d/%m/%Y %H:%M:%S")
@@ -116,8 +99,6 @@ def evaluate_imputation(
     sd_true = tsinfer.load(in_true_samples_file)
     ts_ref = tskit.load(in_reference_trees_file)
 
-    ts_anc = tsinfer.eval_util.make_ancestors_ts(ts=ts_ref, remove_leaves=remove_leaves)
-
     data_imputed_site_pos = (
         data_imputed.sites_position
         if in_file_type == "trees"
@@ -125,12 +106,13 @@ def evaluate_imputation(
     )
     sd_true_site_pos = sd_true.sites_position[:]
     ts_ref_site_pos = ts_ref.sites_position
-    ts_anc_site_pos = ts_anc.sites_position
 
     # Define chip and mask sites relative to the ancestors ts from the reference ts.
     chip_site_pos_all = masks.parse_site_position_file(in_chip_file)
-    chip_site_pos = np.sort(list(set(ts_anc_site_pos) & set(chip_site_pos_all)))
-    mask_site_pos = set(ts_anc_site_pos) - set(chip_site_pos)  # Must NOT be in chip set
+    chip_site_pos = np.sort(
+        list(set(ts_ref_site_pos) & set(chip_site_pos_all)))
+    mask_site_pos = set(ts_ref_site_pos) - \
+        set(chip_site_pos)  # Must NOT be in chip set
     mask_site_pos = mask_site_pos & set(sd_true_site_pos)  # Must be in ground-truth set
     mask_site_pos = np.sort(
         list(mask_site_pos & set(data_imputed_site_pos))
@@ -139,20 +121,17 @@ def evaluate_imputation(
     assert set(mask_site_pos).issubset(set(data_imputed_site_pos))
     assert set(mask_site_pos).issubset(set(sd_true_site_pos))
     assert set(mask_site_pos).issubset(set(ts_ref_site_pos))
-    assert set(mask_site_pos).issubset(set(ts_anc_site_pos))
 
     vars_data_imputed = data_imputed.variants()
     vars_sd_true = sd_true.variants()
     vars_ts_ref = ts_ref.variants()
-    vars_ts_anc = ts_anc.variants()
 
     v_data_imputed = next(vars_data_imputed)
     v_sd_true = next(vars_sd_true)
     v_ts_ref = next(vars_ts_ref)
-    v_ts_anc = next(vars_ts_anc)
 
     # Get a simplified ts for arity calculations.
-    ts_ref_simp = ts_ref.simplify()
+    #ts_ref_simp = ts_ref.simplify()
 
     results = None
     for pos in tqdm.tqdm(mask_site_pos):
@@ -162,8 +141,6 @@ def evaluate_imputation(
             v_sd_true = next(vars_sd_true)
         while v_ts_ref.site.position != pos:
             v_ts_ref = next(vars_ts_ref)
-        while v_ts_anc.site.position != pos:
-            v_ts_anc = next(vars_ts_anc)
 
         # Variant objects have ordered lists of alleles.
         ref_ancestral_allele = v_ts_ref.alleles[0]  # Denoted by 0
@@ -179,10 +156,6 @@ def evaluate_imputation(
         ref_af_1 = ref_freqs[ref_derived_allele]
         ref_ma_index = 1 if ref_af_1 < ref_af_0 else 0
         ref_ma_freq = ref_af_1 if ref_af_1 < ref_af_0 else ref_af_0
-
-        # Skip sites with a MAF too low.
-        if ref_ma_freq < min_maf:
-            continue
 
         # Get Minor Allele index and frequency from `data_imputed`.
         if in_file_type == "trees":
@@ -204,10 +177,6 @@ def evaluate_imputation(
             imputed_ma_freq = float("nan")
 
         imputed_genotypes = v_data_imputed.genotypes
-        if flip_alleles:
-            # Flip alleles before computing IQS.
-            # This is useful for checking whether the ancestral allele is wrong.
-            imputed_genotypes = np.where(imputed_genotypes == 0, 1, 0)
 
         # Calculate imputation performance metrics.
         iqs = measures.compute_iqs(
@@ -215,10 +184,6 @@ def evaluate_imputation(
             gt_imputed=imputed_genotypes,
             ploidy=2,
         )
-
-        # Skip sites with an IQS below or above a specified threshold.
-        if iqs < min_iqs or iqs > max_iqs:
-            continue
 
         # Obtain site information.
         # Get the mutations at this site.
@@ -281,24 +246,18 @@ def evaluate_imputation(
                 "#" + "in_imputed_file" + "=" + f"{in_imputed_file}",
                 "#" + "in_true_samples_file" + "=" + f"{in_true_samples_file}",
                 "#" + "in_reference_trees_file" + "=" + f"{in_reference_trees_file}",
-                "#" + "remove_leaves" + "=" + f"{remove_leaves}",
                 "#" + "in_chip_file" + "=" + f"{in_chip_file}",
                 "#" + "out_csv_file" + "=" + f"{out_csv_file}",
-                "#" + "min_maf" + "=" + f"{min_maf}",
-                "#" + "min_iqs" + "=" + f"{min_iqs}",
-                "#" + "max_iqs" + "=" + f"{max_iqs}",
-                "#" + "flip_alleles" + "=" + f"{flip_alleles}",
                 # Site statistics
                 "#" + "num_sites_data_imputed" + "=" + f"{len(data_imputed_site_pos)}",
                 "#" + "num_sites_sd_true" + "=" + f"{len(sd_true_site_pos)}",
                 "#" + "num_sites_ts_ref" + "=" + f"{len(ts_ref_site_pos)}",
-                "#" + "num_sites_ts_anc" + "=" + f"{len(ts_anc_site_pos)}",
                 "#" + "num_chip_sites_all" + "=" + f"{len(chip_site_pos_all)}",
-                "#" + "num_chip_sites" + "=" + f"{len(chip_site_pos)}",  # in ts_anc
+                "#" + "num_chip_sites" + "=" + f"{len(chip_site_pos)}",
                 "#"
                 + "num_mask_sites"
                 + "="
-                + f"{len(mask_site_pos)}",  # in ts_anc and data_imputed
+                + f"{len(mask_site_pos)}",  # in ts_ref and data_imputed
             ]
         )
         + "\n"
