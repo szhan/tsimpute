9242f9f2321915c1750969270d348a36eae5e256
Shing Zhan <shing.zhan@gmail.com>
2022-11-02 14:22:44 +0000

    Reformat and polish

--- src/perform_imputation_by_sample_matching.py
+++ src/perform_imputation_by_sample_matching.py
@@ -64,28 +64,30 @@ def get_traceback_path(
     return path
 
 
-def impute_by_sample_matching(ts, sd, recombination_rate, mutation_rate, precision):
-    assert ts.num_sites == sd.num_sites
-    assert np.all(np.isin(ts.sites_position, sd.sites_position))
+def impute_by_sample_matching(
+    tree_sequence, sample_data, recombination_rate, mutation_rate, precision
+):
+    assert tree_sequence.num_sites == sample_data.num_sites
+    assert np.all(np.isin(tree_sequence.sites_position, sample_data.sites_position))
 
     logging.info("Mapping samples to ACGT space.")
-    H1 = np.zeros((ts.num_sites, sd.num_samples), dtype=np.int32)
+    H1 = np.zeros((tree_sequence.num_sites, sample_data.num_samples), dtype=np.int32)
     i = 0
-    for v in tqdm(sd.variants()):
-        if v.site.position in ts.sites_position:
+    for v in tqdm(sample_data.variants()):
+        if v.site.position in tree_sequence.sites_position:
             H1[i, :] = create_index_map(v.alleles)[v.genotypes]
             i += 1
     H1 = H1.T
 
     logging.info("Performing traceback.")
     # TODO: Pass in arrays without creating them.
-    recombination_rate_map = np.repeat(recombination_rate, ts.num_sites)
-    mutation_rate_map = np.repeat(mutation_rate, ts.num_sites)
+    recombination_rate_map = np.repeat(recombination_rate, tree_sequence.num_sites)
+    mutation_rate_map = np.repeat(mutation_rate, tree_sequence.num_sites)
 
-    H2 = np.zeros((sd.num_samples, ts.num_sites), dtype=np.int32)
-    for i in tqdm(np.arange(sd.num_samples)):
+    H2 = np.zeros((sample_data.num_samples, tree_sequence.num_sites), dtype=np.int32)
+    for i in tqdm(np.arange(sample_data.num_samples)):
         H2[i, :] = get_traceback_path(
-            tree_sequence=ts,
+            tree_sequence=tree_sequence,
             haplotype=H1[i, :],
             recombination_rate_map=recombination_rate_map,
             mutation_rate_map=mutation_rate_map,
@@ -94,8 +96,8 @@ def impute_by_sample_matching(ts, sd, recombination_rate, mutation_rate, precisi
 
     logging.info("Imputing into samples.")
     i = 0
-    H3 = np.zeros((sd.num_samples, ts.num_sites), dtype=np.int32)
-    for v in tqdm(ts.variants()):
+    H3 = np.zeros((sample_data.num_samples, tree_sequence.num_sites), dtype=np.int32)
+    for v in tqdm(tree_sequence.variants()):
         H3[:, i] = v.genotypes[H2[:, i]]
         i += 1
 
@@ -103,19 +105,19 @@ def impute_by_sample_matching(ts, sd, recombination_rate, mutation_rate, precisi
 
 
 def write_genotype_matrix_to_samples(
-    ts,
+    tree_sequence,
     genotype_matrix,
     mask_site_pos,
     chip_site_pos,
     out_file,
 ):
-    assert ts.num_sites == genotype_matrix.shape[1]
+    assert tree_sequence.num_sites == genotype_matrix.shape[1]
 
     out_file = str(out_file)
-    ts_iter = ts.variants()
+    ts_iter = tree_sequence.variants()
 
     i = 0  # Track iterating through `genotype_matrix`
-    with tsinfer.SampleData(path=out_file) as sd:
+    with tsinfer.SampleData(path=out_file) as sample_data:
         for ts_v in tqdm(ts_iter):
             # Set metadata
             metadata = {"marker": ""}
@@ -123,9 +125,8 @@ def write_genotype_matrix_to_samples(
                 metadata["marker"] = "mask"
             elif ts_v.site.position in chip_site_pos:
                 metadata["marker"] = "chip"
-
             # Add site
-            sd.add_site(
+            sample_data.add_site(
                 position=ts_v.site.position,
                 genotypes=genotype_matrix[:, i],
                 alleles=ts_v.alleles,
@@ -178,9 +179,10 @@ def perform_imputation_by_sample_matching(
     precision,
 ):
     out_dir = Path(out_dir)
-    out_samples_file = out_dir / f"{out_prefix}.imputed.samples"
-    tmp_samples_file = out_dir / f"{out_prefix}.tmp.samples"
     log_file = out_dir / f"{out_prefix}.log"
+    compat_samples_file = out_dir / f"{out_prefix}.compat.samples"
+    out_samples_file = out_dir / f"{out_prefix}.imputed.samples"
+
     logging.basicConfig(filename=str(log_file), encoding="utf-8", level=logging.INFO)
 
     logging.info(f"Loading reference trees file {in_reference_trees_file}")
@@ -205,17 +207,17 @@ def perform_imputation_by_sample_matching(
 
     assert (
         len(set(chip_site_pos) & set(mask_site_pos)) == 0
-    ), f"Chip and mask site positions are not mutually exclusive."
+    ), f"Chip and mask site positions are not mutually exclusive"
 
     logging.info("Making samples compatible with the reference trees")
-    logging.info(f"Writing compatible samples to file: {tmp_samples_file}")
+    logging.info(f"Writing compatible samples to file: {compat_samples_file}")
     sd_compat = util.make_compatible_sample_data(
         sd_target,
         ts_ref,
         skip_unused_markers=True,
         chip_site_pos=chip_site_pos,  # Site metadata
         mask_site_pos=mask_site_pos,  # Site metadata
-        path=str(tmp_samples_file),
+        path=str(compat_samples_file),
     )
 
     logging.info("Imputing into target samples")
@@ -227,7 +229,7 @@ def perform_imputation_by_sample_matching(
         precision=precision,
     )
 
-    logging.info(f"Writing imputed samles to file: {out_samples_file}")
+    logging.info(f"Writing imputed samples to file: {out_samples_file}")
     write_genotype_matrix_to_samples(
         ts=ts_ref,
         genotype_matrix=gm_imputed,
