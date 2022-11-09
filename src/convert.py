e0ca5f3842cfb12a33f47b2496fbabd0c30eb002
Shing Zhan <shing.zhan@gmail.com>
2022-11-09 15:20:24 +0000

    Remove unneeded code

--- src/convert.py
+++ src/convert.py
@@ -410,9 +410,6 @@ class VcfConverter(Converter):
                 ancestral_state = row.REF
             site = self.convert_genotypes(row, ancestral_state, exclude_indels)
             if site is not None:
-                # Mark sites to list in `exclude_position``
-                # when calling `generate_ancestors()`.
-                site.metadata["exclude_position"] = True
                 self.samples.add_site(
                     position=site.position,
                     genotypes=site.genotypes,
