8ae95a05602323ba86e0af99213e8c0076d5fd36
szhan <shing.zhan@gmail.com>
2025-03-23 13:05:39 +0000

    Raise errors

--- tsimpute/beagle_numba.py
+++ tsimpute/beagle_numba.py
@@ -29,27 +29,25 @@ class GeneticMap:
 
 
 # Helper functions.
-def check_data(ref_h, query_h):
+def is_data_compatible(ref_h, query_h):
     """
     For each position, check whether the alleles in the query haplotype
     are represented in the reference haplotypes.
 
-    Missing data (i.e. -1) are ignored.
-
     :param numpy.ndarray ref_h: Reference haplotypes.
-    :param numpy.ndarray query_h: One query haplotype.
+    :param numpy.ndarray query_h: A query haplotype.
     :return: True if alleles in query are in references at all the positions.
     :rtype: bool
     """
-    m = ref_h.shape[0]  # Number of genotyped positions.
-    assert m == len(query_h), "Reference and query haplotypes differ in length."
-    for i in range(m):
-        if query_h[i] != tskit.MISSING_DATA:
-            ref_a = np.unique(ref_h[i, :])
-            if query_h[i] not in ref_a:
-                raise AssertionError(
-                    f"Allele {query_h[i]} at the {i}-th position is not in reference."
-                )
+    num_sites = ref_h.shape[0]
+    if num_sites != len(query_h):
+        raise ValueError("Incompatible reference and query haplotypes.")
+    for i in range(num_sites):
+        if query_h[i] == tskit.MISSING_DATA:
+            continue
+        if ~np.any(ref_h[i, :] == query_h[i]):
+            # f"Allele {query_h[i]} at {i}-th position is not in reference."
+            return False
     return True
 
 
@@ -76,25 +74,27 @@ def convert_to_cm(site_pos, genetic_map=None):
     _MIN_CM_DIST = 1e-7
     if genetic_map is None:
         return site_pos / 1e6  # 1 cM / 1 Mb
-    assert np.all(site_pos >= genetic_map.base_pos[0]) and np.all(
-        site_pos < genetic_map.base_pos[-1]
-    ), "Some site positions are outside of the genetic map."
+    if ~np.all(site_pos >= genetic_map.base_pos[0]) or \
+        ~np.all(site_pos < genetic_map.base_pos[-1]):
+        raise ValueError("Site positions are outside of the genetic map.")
     # Approximate genetic map distances by linear interpolation.
     # Note np.searchsorted(a, v, side='right') returns i s.t. a[i-1] <= v < a[i].
     right_idx = np.searchsorted(genetic_map.base_pos, site_pos, side="right")
-    m = len(site_pos)
-    est_cm = np.zeros(m, dtype=np.float64)  # BEAGLE 4.1 uses double in Java.
-    for i in range(m):
+    num_sites = len(site_pos)
+    est_cm = np.zeros(num_sites, dtype=np.float64)  # BEAGLE 4.1 uses double in Java.
+    for i in range(num_sites):
         a = genetic_map.base_pos[right_idx[i] - 1]
         b = genetic_map.base_pos[right_idx[i]]
         fa = genetic_map.gen_pos[right_idx[i] - 1]
         fb = genetic_map.gen_pos[right_idx[i]]
-        assert (
-            site_pos[i] >= a
-        ), f"Query position is not >= left-bound position: {site_pos[i]}, {a}."
-        assert (
-            fb >= fa
-        ), f"Genetic map positions are not monotonically ascending: {fb}, {fa}."
+        if site_pos[i] < a:
+            raise ValueError(
+                f"Query position is not >= left-bound position: {site_pos[i]}, {a}."
+            )
+        if fb < fa:
+            raise ValueError(
+                f"Genetic map positions are not monotonically ascending: {fb}, {fa}."
+            )
         est_cm[i] = fa
         est_cm[i] += (fb - fa) * (site_pos[i] - a) / (b - a)
         # Ensure that adjacent positions are not identical in cM.
@@ -132,7 +132,7 @@ def get_mismatch_probs(num_sites, error_rate):
 
 def get_transition_probs(cm, h, ne):
     """
-    Compute probabilities of transitioning to a random state at genotyped positions.
+    Compute transition probabilities to a random state at genotyped positions.
 
     In BEAGLE 4.1, the default value of `ne` is set to 1e6,
     whereas in BEAGLE 5.4, the default value of `ne` is set to 1e5.
@@ -154,7 +154,8 @@ def get_transition_probs(cm, h, ne):
     :rtype: numpy.ndarray
     """
     # E(number of crossover events) at first site is always 0.
-    r = np.zeros(len(cm), dtype=np.float64)
+    num_sites = len(cm)
+    r = np.zeros(num_sites, dtype=np.float64)
     r[1:] = np.diff(cm)
     c = -0.04 * (ne / h)
     trans_probs = -1 * np.expm1(c * r)
@@ -185,7 +186,12 @@ def compute_emission_probability(mismatch_prob, ref_a, query_a, num_alleles=2):
 # Replication of BEAGLE's implementation of LS HMM forward-backward algorithm.
 @njit
 def compute_forward_matrix_beaglelike(
-    ref_h, query_h, trans_probs, mismatch_probs, *, num_alleles=2
+    ref_h,
+    query_h,
+    trans_probs,
+    mismatch_probs,
+    *,
+    num_alleles=2,
 ):
     """
     Implement LS HMM forward algorithm as in BEAGLE.
@@ -203,18 +209,18 @@ def compute_forward_matrix_beaglelike(
     :return: Forward probability matrix.
     :rtype: numpy.ndarray
     """
-    h = ref_h.shape[1]  # Number of reference haplotypes.
-    m = ref_h.shape[0]  # Number of genotyped positions.
-    assert len(query_h) == m
-    fwd_mat = np.zeros((m, h), dtype=np.float64)
+    num_sites, num_haps = ref_h.shape
+    if len(query_h) != num_sites:
+        raise ValueError("Incompatible query haplotype.")
+    fwd_mat = np.zeros((num_sites, num_haps), dtype=np.float64)
     last_sum = 1.0  # Normalization factor.
-    for i in range(m):
+    for i in range(num_sites):
         # Get site-specific parameters.
-        shift = trans_probs[i] / h
+        shift = trans_probs[i] / num_haps
         scale = (1 - trans_probs[i]) / last_sum
         # Get allele at genotyped position i on query haplotype.
         query_a = query_h[i]
-        for j in range(h):
+        for j in range(num_haps):
             # Get allele at genotyped position i on reference haplotype j.
             ref_a = ref_h[i, j]
             # Get emission probability.
@@ -226,22 +232,28 @@ def compute_forward_matrix_beaglelike(
                 fwd_mat[i, j] *= scale * fwd_mat[i - 1, j] + shift
         site_sum = np.sum(fwd_mat[i, :])
         # Prior probabilities are multiplied when i = 0.
-        last_sum = site_sum / h if i == 0 else site_sum
+        last_sum = site_sum / num_haps if i == 0 else site_sum
     return fwd_mat
 
 
 @njit
 def compute_forward_matrix(
-    ref_h, query_h, trans_probs, mismatch_probs, *, num_alleles=2
+    ref_h,
+    query_h,
+    trans_probs,
+    mismatch_probs,
+    *,
+    num_alleles=2,
 ):
-    m, h = ref_h.shape
-    assert len(query_h) == m
-    fwd_mat = np.zeros((m, h), dtype=np.float64)
+    num_sites, num_haps = ref_h.shape
+    if len(query_h) != num_sites:
+        raise ValueError("Incompatible query haplotype.")
+    fwd_mat = np.zeros((num_sites, num_haps), dtype=np.float64)
     last_sum = 1.0
-    for i in range(m):
-        shift = trans_probs[i] / h
+    for i in range(num_sites):
+        shift = trans_probs[i] / num_haps
         scale = (1 - trans_probs[i]) / last_sum
-        for j in range(h):
+        for j in range(num_haps):
             fwd_mat[i, j] = compute_emission_probability(
                 mismatch_prob=mismatch_probs[i],
                 ref_a=ref_h[i, j],
@@ -251,13 +263,18 @@ def compute_forward_matrix(
             if i > 0:
                 fwd_mat[i, j] *= scale * fwd_mat[i - 1, j] + shift
         site_sum = np.sum(fwd_mat[i, :])
-        last_sum = site_sum / h if i == 0 else site_sum
+        last_sum = site_sum / num_haps if i == 0 else site_sum
     return fwd_mat
 
 
 @njit
 def compute_backward_matrix_beaglelike(
-    ref_h, query_h, trans_probs, mismatch_probs, *, num_alleles=2
+    ref_h,
+    query_h,
+    trans_probs,
+    mismatch_probs,
+    *,
+    num_alleles=2,
 ):
     """
     Implement LS HMM backward algorithm as in BEAGLE.
@@ -278,15 +295,15 @@ def compute_backward_matrix_beaglelike(
     :return: Backward probability matrix.
     :rtype: numpy.ndarray
     """
-    h = ref_h.shape[1]  # Number of reference haplotypes.
-    m = ref_h.shape[0]  # Number of genotyped positions.
-    assert len(query_h) == m
-    bwd_mat = np.zeros((m, h), dtype=np.float64)
-    bwd_mat[-1, :] = 1.0 / h  # Initialise the last column.
-    for i in range(m - 2, -1, -1):
+    num_sites, num_haps = ref_h.shape
+    if len(query_h) != num_sites:
+        raise ValueError("Incompatible query haplotype.")
+    bwd_mat = np.zeros((num_sites, num_haps), dtype=np.float64)
+    bwd_mat[-1, :] = 1.0 / num_haps
+    for i in range(num_sites - 2, -1, -1):
         iP1 = i + 1
         query_a = query_h[iP1]
-        for j in range(h):
+        for j in range(num_haps):
             ref_a = ref_h[iP1, j]
             em_prob = mismatch_probs[iP1]
             if query_a == ref_a:
@@ -294,22 +311,28 @@ def compute_backward_matrix_beaglelike(
             bwd_mat[iP1, j] *= em_prob
         site_sum = np.sum(bwd_mat[iP1, :])
         scale = (1 - trans_probs[iP1]) / site_sum
-        shift = trans_probs[iP1] / h
+        shift = trans_probs[iP1] / num_haps
         bwd_mat[i, :] = scale * bwd_mat[iP1, :] + shift
     return bwd_mat
 
 
 @njit
 def compute_backward_matrix(
-    ref_h, query_h, trans_probs, mismatch_probs, *, num_alleles=2
+    ref_h,
+    query_h,
+    trans_probs,
+    mismatch_probs,
+    *,
+    num_alleles=2,
 ):
-    m, h = ref_h.shape
-    assert len(query_h) == m
-    bwd_mat = np.zeros((m, h), dtype=np.float64)
-    bwd_mat[-1, :] = 1.0 / h
-    for i in range(m - 2, -1, -1):
+    num_sites, num_haps = ref_h.shape
+    if len(query_h) != num_sites:
+        raise ValueError("Incompatible query haplotype.")
+    bwd_mat = np.zeros((num_sites, num_haps), dtype=np.float64)
+    bwd_mat[-1, :] = 1.0 / num_haps
+    for i in range(num_sites - 2, -1, -1):
         iP1 = i + 1
-        for j in range(h):
+        for j in range(num_haps):
             bwd_mat[iP1, j] *= compute_emission_probability(
                 mismatch_prob=mismatch_probs[iP1],
                 ref_a=ref_h[iP1, j],
@@ -318,7 +341,7 @@ def compute_backward_matrix(
             )
         site_sum = np.sum(bwd_mat[iP1, :])
         scale = (1 - trans_probs[iP1]) / site_sum
-        shift = trans_probs[iP1] / h
+        shift = trans_probs[iP1] / num_haps
         bwd_mat[i, :] = scale * bwd_mat[iP1, :] + shift
     return bwd_mat
 
@@ -333,17 +356,16 @@ def compute_state_prob_matrix(fwd_mat, bwd_mat):
     :return: Posterior state probability matrix.
     :rtype: numpy.ndarray
     """
-    assert (
-        fwd_mat.shape == bwd_mat.shape
-    ), "Incompatible forward and backward matrices."
-    m, h = fwd_mat.shape
+    if fwd_mat.shape != bwd_mat.shape:
+        raise ValueError("Incompatible forward and backward matrices.")
+    num_sites, num_haps = fwd_mat.shape
     state_mat = np.zeros_like(fwd_mat)
-    for i in range(m):
+    for i in range(num_sites):
         site_sum = 0
-        for j in range(h):
+        for j in range(num_haps):
             state_mat[i, j] = fwd_mat[i, j] * bwd_mat[i, j]
             site_sum += state_mat[i, j]
-        for j in range(h):
+        for j in range(num_haps):
             state_mat[i, j] /= site_sum
     return state_mat
 
@@ -435,13 +457,14 @@ def interpolate_allele_probs(
     :rtype: tuple(numpy.ndarray, numpy.ndarray)
     """
     # TODO: Allow for biallelic site matrix. Work with `_tskit.lshmm` properly.
-    alleles = np.arange(len(tskit.ALLELES_ACGT))
+    num_alleles = len(tskit.ALLELES_ACGT)
+    alleles = np.arange(num_alleles)
     m = state_mat.shape[0]  # Number of genotyped positions.
     x = ref_h.shape[0]  # Number of ungenotyped positions.
     # Set threshold to set trivial probabilities to zero.
     _MIN_THRESHOLD = 0
     weights, right_idx = get_weights(pos_typed, pos_untyped, cm_typed, cm_untyped)
-    probs = np.zeros((x, len(alleles)), dtype=np.float64)
+    probs = np.zeros((x, num_alleles), dtype=np.float64)
     for i in range(x):
         k = right_idx[i]
         w = weights[i]
@@ -457,13 +480,15 @@ def interpolate_allele_probs(
                 _MIN_THRESHOLD = min(0.005, threshold_Am)
             if k == 0:
                 # See 'setFirstAlleleProbs' in 'LSHapBaum.java'.
-                assert w == 1.0, "Weight should be 1.0."
+                if w != 1.0:
+                    raise ValueError("Weight must be 1.")
                 sum_probs_a_k = np.sum(state_mat[k, is_a_in_ref_h])
                 if sum_probs_a_k > _MIN_THRESHOLD:
                     probs[i, a] += sum_probs_a_k
             elif k == m:
                 # See 'setLastAlleleProbs' in 'LSHapBaum.java'.
-                assert w == 0.0, "Weight should be 0.0."
+                if w != 0.0:
+                    raise ValueError("Weight must be 0.")
                 sum_probs_a_kM1 = np.sum(state_mat[k - 1, is_a_in_ref_h])
                 if sum_probs_a_kM1 > _MIN_THRESHOLD:
                     probs[i, a] += sum_probs_a_kM1
@@ -475,7 +500,8 @@ def interpolate_allele_probs(
                     probs[i, a] += w * sum_probs_a_kM1
                     probs[i, a] += (1 - w) * sum_probs_a_k
     site_sums = np.sum(probs, axis=1)
-    assert np.all(site_sums > 0), "Some site sums of allele probabilities is <= 0."
+    if ~np.all(site_sums > 0):
+        raise ValueError("Site sums of allele probabilities is <= 0.")
     probs_rescaled = probs / site_sums[:, np.newaxis]
     if return_weights:
         return (probs_rescaled, weights)
@@ -499,7 +525,8 @@ def get_map_alleles(allele_probs, num_alleles):
     :rtype: tuple(numpy.ndarray, numpy.ndarray)
     """
     x, a = allele_probs.shape
-    assert a == num_alleles
+    if a != num_alleles:
+        raise ValueError("Unexpected number of alleles.")
     imputed_alleles = np.zeros(x, dtype=np.int32) - 1
     max_allele_probs = np.zeros(x, dtype=np.float64)
     for i in range(x):
@@ -528,7 +555,7 @@ def run_interpolation_beagle(
 
     Reference haplotypes and query haplotype are of size (m + x, h) and (m + x).
 
-    The physical positions of all the markers are an array of size (m + x).
+    The site positions of all the markers are an array of size (m + x).
 
     This produces imputed alleles and their probabilities at the ungenotyped positions
     of the query haplotype.
@@ -537,8 +564,8 @@ def run_interpolation_beagle(
     In BEAGLE 5.4, the default value of `ne` is 1e5, and `error_rate` is data-dependent.
 
     :param numpy.ndarray ref_h: Reference haplotypes.
-    :param numpy.ndarray query_h: One query haplotype.
-    :param numpy.ndarray pos_all: Physical positions of all the markers (bp).
+    :param numpy.ndarray query_h: A query haplotype.
+    :param numpy.ndarray pos_all: Site positions of all the markers (bp).
     :param int ne: Effective population size (default = 1e6).
     :param float error_rate: Allele error rate (default = 1e-4).
     :param GeneticMap genetic_map: Genetic map (default = None).
@@ -596,5 +623,7 @@ def run_interpolation_beagle(
         use_threshold=use_threshold,
         return_weights=False,
     )
-    imputed_alleles, max_allele_probs = get_map_alleles(imputed_allele_probs, num_alleles=num_alleles)
+    imputed_alleles, max_allele_probs = get_map_alleles(
+        imputed_allele_probs, num_alleles=num_alleles
+    )
     return (imputed_alleles, max_allele_probs)
