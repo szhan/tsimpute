37591df6a4260284998f922e6900ac32db7fc59a
szhan <shing.zhan@gmail.com>
2025-03-23 10:53:01 +0000

    Move notes to a Markdown file

--- tsimpute/beagle_numba.py
+++ tsimpute/beagle_numba.py
@@ -1,48 +1,4 @@
-"""
-Implementation of the BEAGLE 4.1 algorithm to impute alleles by linear interpolation
-of state probabilities at ungenotyped positions in the hidden state probability matrix.
-
-This was implemented while closely consulting the BEAGLE 4.1 paper and source code:
-* Browning & Browning (2016). Am J Hum Genet 98:116-126. doi:10.1016/j.ajhg.2015.11.020
-* Source code: https://faculty.washington.edu/browning/beagle/b4_1.html
-
-These notations are used throughout:
-h = number of reference haplotypes.
-m = number of genotyped positions.
-x = number of ungenotyped positions.
-
-This implementation takes the following inputs:
-* Panel of reference haplotypes in a matrix of size (m + x, h).
-* Query haplotype in an array of size (m + x).
-* Physical positions of all the markers in an array of size (m + x).
-* Genetic map.
-
-In the query haplotype:
-* Genotyped positions take values of 0, 1, 2, or 3 (i.e. ACGT encoding).
-* Ungenotyped positions take -1.
-
-The following objects are computed:
-* Forward and backward probability matrices of size (m, h).
-* Hidden state probability matrix of size (m, h).
-* Interpolated state probability matrix of size (x, h).
-* Imputed allele probability matrix of size (x, 4),
-* Imputed alleles as the maximum a posteriori alleles.
-
-The following evaluation metrics are produced in VCF format:
-* Estimated allelic R-squared (AR2).
-* Dosage R-squared (DR2).
-* Estimated allele frequency (AF).
-* Genotype probabilities of 00, 01/10, and 11 (GP).
-* Estimated dosage (DS).
-
-To improve computational efficiency, BEAGLE uses aggregated markers, which are clusters
-of markers within a 0.005 cM interval (default). Because the genotypes are phased,
-the alleles in the aggregated markers form distinct "allele sequences". Below, we do not
-use aggregated markers or allele sequences, which would complicate the implementation.
-
-Rather than exactly replicate the original BEAGLE algorithm, this implementation uses
-Equation 1 of BB2016.
-"""
+"""Re-implementation of the BEAGLE 4.1 algorithm"""
 import warnings
 from dataclasses import dataclass
 
